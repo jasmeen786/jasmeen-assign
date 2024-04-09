f77bbf2872f9fd6ce5e0e7e11fbc5e8e44b5bf0d
jasmeen786 <148907683+jasmeen786@users.noreply.github.com>
2024-04-08 22:42:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const requestBody = Object.fromEntries(formData);
 
         try {
-            const response = await fetch('http://localhost:7000/register', {
+            const response = await fetch('https://my-backend-project-lt0e.onrender.com/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
